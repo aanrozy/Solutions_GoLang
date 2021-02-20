17413152861a6ff1441ce430cbad3421ff853a6f
aanrozy <30997157+aanrozy@users.noreply.github.com>
2021-02-20 17:52:48 +0700

    added question

--- Medium/sort_colors.go
+++ Medium/sort_colors.go
@@ -1,3 +1,7 @@
+// question
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+
 func sortColors(nums []int)  {
     for i := 1; i < len(nums); i++ {
         for j := 0; j < len(nums) - 1; j++ {
