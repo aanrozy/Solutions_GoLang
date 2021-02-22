3adbbda860d5ae4cf9ba04bcd75e19b92e91edf8
aanrozy <30997157+aanrozy@users.noreply.github.com>
2021-02-22 22:09:35 +0700

    Question added

--- Easy/remove_duplicates.go
+++ Easy/remove_duplicates.go
@@ -1,3 +1,17 @@
+/*
+Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Clarification:
+Confused why the returned value is an integer but your answer is an array?
+Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+Internally you can think of this:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2]
+Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+*/
+
 func removeDuplicates(nums []int) int {
     i := 0
     for j := 1; j < len(nums); j++ {
